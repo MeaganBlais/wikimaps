4599bf2c0d2d6ce143b069cc1e2a7bb681830b54
Vasiliy Klimkin <vasily.klimkin@gmail.com>
2018-06-13 17:02:52 -0400

    Debugging map

--- src/components/Pages/Maps/Create.js
+++ src/components/Pages/Maps/Create.js
@@ -4,22 +4,35 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 class MapsCreate extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedPlace: {
+       name: ''
+      },
+
+    }
+  }
+
 
   render() {
+
     return (
     <div>
-      <h1>Create Map</h1>
+      <h1 className="red">Create Map</h1>
+      <div className="mapSize">
+        <Map google={this.props.google} zoom={14}>
 
-      <Map google={this.props.google} zoom={14}>
+          <Marker onClick={this.onMarkerClick}
+                  name={'Current location'} />
 
-        <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
-
-        <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-            </div>
-        </InfoWindow>
-      </Map>
+          <InfoWindow onClose={this.onInfoWindowClose}>
+              <div>
+                <h1>{this.state.selectedPlace.name}</h1>
+              </div>
+          </InfoWindow>
+        </Map>
+      </div>
 
       <NavLink to="/">Go Home</NavLink>
     </div>
